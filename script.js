65b79f915604f9a665141f2927ddcf2056da252e
shefy123 <38942966+shefy123@users.noreply.github.com>
2018-10-18 17:37:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+  let options = {
+  	duration :200
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  });
+
+  
